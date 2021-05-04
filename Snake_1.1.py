47f4802b0645ebbda355a078f54d85d6795e177e
Foxy05 <82767271+Foxy05@users.noreply.github.com>
2021-05-04 18:58:25 +0200

    Add files via upload
    Nový Gamemode

--- Snake_1.1.py
+++ Snake_1.1.py
@@ -45,6 +45,10 @@ def text_obrazovky(text, farba, x, y):
 def text_obrazovky2(text, farba, x, y):
     text_obrazovky2 = font2.render(text, True, farba)
     screen.blit(text_obrazovky2, [x,y])
+#PAUSE
+def pause():
+    screen.blit(bg, (0, 0))
+    text_obrazovky2("PAUSE", farba_cierna, 180, 250)
 
 #HAD
 def snake (screen, farba, list, velkost_hada):
@@ -153,8 +157,14 @@ def game2():
     velocity_y = 0
     list = []
     dlzka = 1
-    jablko_x = random.randint(20, sirka - 20)
-    jablko_y = random.randint(60, vyska - 20)
+    jablko1_x = random.randint(20, sirka - 20)
+    jablko1_y = random.randint(60, vyska - 20)
+    jablko2_x = random.randint(20, sirka - 20)
+    jablko2_y = random.randint(60, vyska - 20)
+    jablko3_x = random.randint(20, sirka - 20)
+    jablko3_y = random.randint(60, vyska - 20)
+    jablko4_x = random.randint(20, sirka - 20)
+    jablko4_y = random.randint(60, vyska - 20)
     score = 0
     init_velocity = 4
     velkost_hada = 30
@@ -162,7 +172,7 @@ def game2():
     while not zrusit_hru:
         if koniec_hry:
             screen.blit(bg, (0, 0))
-            text_obrazovky2("GAME OVER", farba_cierna, 180, 250)
+            text_obrazovky2("  GAME OVER", farba_cierna, 180, 250)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -170,7 +180,7 @@ def game2():
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
-                        game()
+                        game2()
 
         else:
 
@@ -198,17 +208,41 @@ def game2():
             snake_x = snake_x + velocity_x
             snake_y = snake_y + velocity_y
 
-            if abs(snake_x - jablko_x) < 10 and abs(snake_y - jablko_y) < 10:
+            if abs(snake_x - jablko1_x) < 10 and abs(snake_y - jablko1_y) < 10:
                 efekt.play()
                 score += 1
-                jablko_x = random.randint(20, sirka - 30)
-                jablko_y = random.randint(60, vyska - 30)
+                jablko1_x = random.randint(20, sirka - 30)
+                jablko1_y = random.randint(60, vyska - 30)
+                dlzka += 5
+
+            if abs(snake_x - jablko2_x) < 10 and abs(snake_y - jablko2_y) < 10:
+                efekt.play()
+                score += 1
+                jablko2_x = random.randint(20, sirka - 30)
+                jablko2_y = random.randint(60, vyska - 30)
+                dlzka += 5
+
+            if abs(snake_x - jablko3_x) < 10 and abs(snake_y - jablko3_y) < 10:
+                efekt.play()
+                score += 1
+                jablko3_x = random.randint(20, sirka - 30)
+                jablko3_y = random.randint(60, vyska - 30)
+                dlzka += 5
+
+            if abs(snake_x - jablko4_x) < 10 and abs(snake_y - jablko4_y) < 10:
+                efekt.play()
+                score += 1
+                jablko4_x = random.randint(20, sirka - 30)
+                jablko4_y = random.randint(60, vyska - 30)
                 dlzka += 5
 
 
             screen.blit(bg, (0, 0))
             text_obrazovky("Score: " + str(score), farba_biela, 10, 5)
-            pygame.draw.rect(screen, farba_cervena, [jablko_x, jablko_y, velkost_hada, velkost_hada])
+            pygame.draw.rect(screen, farba_cervena, [jablko1_x, jablko1_y, velkost_hada, velkost_hada])
+            pygame.draw.rect(screen, farba_cervena, [jablko2_x, jablko2_y, velkost_hada, velkost_hada])
+            pygame.draw.rect(screen, farba_cervena, [jablko3_x, jablko3_y, velkost_hada, velkost_hada])
+            pygame.draw.rect(screen, farba_cervena, [jablko4_x, jablko4_y, velkost_hada, velkost_hada])
             pygame.draw.line(screen, farba_cierna, (0, 40), (800, 40), 5)
 
             hlava = []
@@ -259,7 +293,7 @@ def game3():
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
-                        game()
+                        game3()
 
         else:
 
@@ -325,9 +359,9 @@ def menu():
     zrusit_hru = False
     while not zrusit_hru:
         screen.blit(bg, (0, 0))
-        text_obrazovky(" QUIT(3)", farba_cervena, 325, 350)
         text_obrazovky("Levely(1)", farba_cervena, 325, 200)
         text_obrazovky("Tabuľka(2)", farba_cervena, 320, 280)
+        text_obrazovky(" QUIT(3)", farba_cervena, 325, 350)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -342,15 +376,15 @@ def menu_levely():
     zrusit_hru = False
     while not zrusit_hru:
         screen.blit(bg, (0, 0))
-        text_obrazovky("Level 1 (1)", farba_cervena, 325, 200)
-        text_obrazovky("Level 2 (2)", farba_cervena, 325, 300)
+        text_obrazovky("Classic (1)", farba_cervena, 325, 200)
+        text_obrazovky("Multi Apple (2)", farba_cervena, 325, 300)
         text_obrazovky("Level 3 (3)", farba_cervena, 325, 400)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_KP2:
-                    game()
                 if event.key == pygame.K_KP1:
+                    game()
+                if event.key == pygame.K_KP2:
                     game2()
                 if event.key == pygame.K_KP3:
                     game3()
